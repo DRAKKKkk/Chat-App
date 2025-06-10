028ea98db8087188d0c17d209e356b5a782ea14f
DRAKKKkk <rajtejas.xyz@gmail.com>
2025-06-10 08:13:00 +0530

    i created

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import { useLocation } from 'react-router-dom';
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+const ENDPOINT = 'http://localhost:5000/';
+
+let socket;
+
+const Chat = () => {
+  const location = useLocation();
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name: parsedName, room: parsedRoom } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(parsedRoom);
+    setName(parsedName);
+
+    socket.emit('join', { name: parsedName, room: parsedRoom }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [location.search]);
+
+  useEffect(() => {
+    socket.on('message', (msg) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+    });
+
+    socket.on('roomData', (data) => {
+      setUsers(data.users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
+
+export default Chat;
